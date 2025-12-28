e7c3d58f108b21530201fd7629e6083d2306fab8
funcieqDEV <bakaj.jakub0@gmail.com>
2025-12-28 10:07:35 +0100

    simple change to test discord webhook

--- compiler/codegen.hpp
+++ compiler/codegen.hpp
@@ -23,6 +23,7 @@ namespace zap
         void generateFunction(const FunDecl &funDecl);
         void generateBody(const BodyNode &body);
         void generateReturn(const ReturnNode &retNode);
+        void genereteLet(const VarDecl &varDel);
         llvm::Value *generateExpression(const ExpressionNode &expr);
         llvm::Type *mapType(const TypeNode &typeNode);
         void emitIRToFile(const std::string &filename);
