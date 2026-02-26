043080360705c1061376a2962daab1644128d899
funcieqDEV <166367015+funcieqDEV@users.noreply.github.com>
2026-02-26 16:30:43 +0100

    Update Doxygen configuration file paths

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,20 +1,19 @@
 option(ZAP_BUILD_REFERENCE "Build ZAP Doxygen Reference" ON)
 
 if(ZAP_BUILD_REFERENCE)
-    find_package(Doxygen)
-    if(Doxygen_FOUND)
-        message(STATUS "Found Doxygen Version "
-            "${DOXYGEN_VERSION} at ${DOXYGEN_EXECUTABLE}")
+    find_package(Doxygen REQUIRED)
+    if(DOXYGEN_FOUND)
+        message(STATUS "Found Doxygen Version ${DOXYGEN_VERSION} at ${DOXYGEN_EXECUTABLE}")
 
-        set(ZAP_DOXYGEN_IN  ${CMAKE_CURRENT_SOURCE_DIR}/Doxygen.in)
+        set(ZAP_DOXYGEN_IN  ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
         set(ZAP_DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
         configure_file(${ZAP_DOXYGEN_IN} ${ZAP_DOXYGEN_OUT} @ONLY)
 
-        message("Building Doxygen")
+        message(STATUS "Building Doxygen")
 
         add_custom_target(zap_doxygen ALL
-            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+            COMMAND ${DOXYGEN_EXECUTABLE} ${ZAP_DOXYGEN_OUT}
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             COMMENT "Generating reference with Doxygen"
             VERBATIM
